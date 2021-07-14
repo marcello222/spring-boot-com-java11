c8058c74de5bf6949718af4bbeb1136c29d68a4c
Marcello <marcelloeduardo222@gmail.com>
2021-07-14 15:51:53 -0300

    Service layer, component registration

--- src/main/java/com/marcello/course/repositories/UserRepository.java
+++ src/main/java/com/marcello/course/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marcello.course.entities.User;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {	
 	
 
