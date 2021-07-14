4181736369d5b9b202eac4a9e116bcc8fc594f64
Marcello <marcelloeduardo222@gmail.com>
2021-07-14 17:35:55 -0300

    Parte 2, Order, Instatn, ISO 8601

---
+++ src/main/java/com/marcello/course/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.marcello.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcello.course.entities.Order;
+import com.marcello.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	
+	public List<Order> findAll() {
+		return repository.findAll();	
+		
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
