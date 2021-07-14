5a68dc59949092d7446e8c60f7820551810ebcce
Marcello <marcelloeduardo222@gmail.com>
2021-07-14 13:52:51 -0300

    Projeto criado

---
+++ src/test/java/com/marcello/course/Springbootjava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcello.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootjava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
