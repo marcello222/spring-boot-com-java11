b290fca9e284e1dc4e8f99775c8b05903f82d6dc
Marcello <marcelloeduardo222@gmail.com>
2021-07-15 16:58:31 -0300

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+---
+
+---
+
 # spring-boot-com-java11 
 
 Projeto Web services com SpringBoot e JPA / Hibernate
+
+
+
+<img src="C:\Users\HP\AppData\Roaming\Typora\typora-user-images\image-20210715165407567.png" alt="image-20210715165407567"  />
--- src/main/java/com/marcello/course/config/TestConfig.java
+++ src/main/java/com/marcello/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.marcello.course.entities.Category;
 import com.marcello.course.entities.Order;
 import com.marcello.course.entities.OrderItem;
+import com.marcello.course.entities.Payment;
 import com.marcello.course.entities.Product;
 import com.marcello.course.entities.User;
 import com.marcello.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 	
