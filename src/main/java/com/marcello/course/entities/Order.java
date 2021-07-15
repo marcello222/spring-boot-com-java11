09df4c85838088f233224b0d60bf65998c5e0bf7
Marcello <marcelloeduardo222@gmail.com>
2021-07-15 18:08:18 -0300

    SubTotal & Total method

--- src/main/java/com/marcello/course/entities/Order.java
+++ src/main/java/com/marcello/course/entities/Order.java
@@ -104,7 +104,13 @@ public class Order implements Serializable {
 		return items;
 	}
 	
-	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
