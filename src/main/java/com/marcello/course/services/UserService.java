714eb13564eb80fc4377e248e98cd203656389e9
Marcello <marcelloeduardo222@gmail.com>
2021-07-16 10:26:02 -0300

    Exception

--- src/main/java/com/marcello/course/services/UserService.java
+++ src/main/java/com/marcello/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcello.course.entities.User;
 import com.marcello.course.repositories.UserRepository;
+import com.marcello.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -24,7 +25,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
